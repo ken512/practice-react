563f251436bc58a240d8912b0b5bbc1ac92ec9fa
porota <ken887265@gmail.com>
2024-05-28 21:17:31 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          課題1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const truncateText = (text, maxLength) => {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + '...';
+  }
+  return text;
 }
 
-export default App;
+const PostsList = ({ src }) => {
+  const maxLength = 65; //省略する際の最大文字数
+
+  // '2023-09-11T09:00:00.000Z'を Dateオブジェクトに変換
+  const date = new Date('2023-09-11T09:00:00.000Z');
+
+  // 年、月、日を数値形式で表示するためのオプションで定義する
+  const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+
+  // `Date` オブジェクトをローカルの日付文字列に変換
+  const dateString = date.toLocaleDateString('ja-JP', options);
+
+  return (
+    <div className="App">
+      <header className="header-App">
+        <a className="link" href='/'>Blog</a>
+        <a className="link" href='/'>お問い合わせ</a>
+      </header>
+
+      {
+        src.map((elem, index) => (
+          <React.Fragment key={index}>
+            <div className="posts-info">
+              <ul >
+                <li>
+                  <a href='/'>
+                    <div className="date">{elem.createdAt = (dateString)}</div>
+                    <div className="programming-language">{elem.categories.map((category, idx) => (
+                      <span key={idx} className="category-box">{category}</span>
+                    ))}</div>
+                    <div className="title">{elem.title}</div>
+                    <div className="content">{truncateText(elem.content, maxLength).split('<br/>').map((line, idx) => (
+                      <React.Fragment key={idx}>
+                        {line}
+                          <br />
+                      </React.Fragment>
+                    ))}</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </React.Fragment>
+        ))}
+    </div>
+  )
+
+}
+
+export default PostsList;
+
