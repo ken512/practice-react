a9d724509c6b850e8899be3fd30596b1e4877f40
porota <ken887265@gmail.com>
2024-05-29 22:04:30 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import './App.css'
 
-const truncateText = (text, maxLength) => {
-  if (text.length > maxLength) {
-    return text.slice(0, maxLength) + '...';
-  }
-  return text;
-}
 
 const PostsList = ({ src }) => {
-  const maxLength = 65; //省略する際の最大文字数
 
-  // '2023-09-11T09:00:00.000Z'を Dateオブジェクトに変換
-  const date = new Date('2023-09-11T09:00:00.000Z');
+  // formatDate 関数を定義し、日付文字列をフォーマットする処理を関数内で行う
+  const formatDate = (dateString) => {
 
-  // 年、月、日を数値形式で表示するためのオプションで定義する
-  const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    // '2023-09-11T09:00:00.000Z'を Dateオブジェクトに変換
+    const date = new Date(dateString);
 
-  // `Date` オブジェクトをローカルの日付文字列に変換
-  const dateString = date.toLocaleDateString('ja-JP', options);
+    // 年、月、日を数値形式で表示するためのオプションで定義する
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+
+    // `Date` オブジェクトをローカルの日付文字列に変換
+    return date.toLocaleDateString('ja-JP', options);
+  }
 
   return (
     <div className="App">
@@ -29,27 +26,21 @@ const PostsList = ({ src }) => {
 
       {
         src.map((elem, index) => (
-          <React.Fragment key={index}>
-            <div className="posts-info">
-              <ul >
-                <li>
-                  <a href='/'>
-                    <div className="date">{elem.createdAt = (dateString)}</div>
-                    <div className="programming-language">{elem.categories.map((category, idx) => (
-                      <span key={idx} className="category-box">{category}</span>
-                    ))}</div>
-                    <div className="title">{elem.title}</div>
-                    <div className="content">{truncateText(elem.content, maxLength).split('<br/>').map((line, idx) => (
-                      <React.Fragment key={idx}>
-                        {line}
-                          <br />
-                      </React.Fragment>
-                    ))}</div>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </React.Fragment>
+          <div key={index} className="posts-info">
+            <ul >
+              <li>
+                <a href='/'>
+                  <div className="date">{formatDate(elem.createdAt)}</div>
+                  <div className="programming-language">{elem.categories.map((category, idx) => (
+                    <span key={idx} className="category-box">{category}</span>
+                  ))}</div>
+                  <div className="title">{elem.title}</div>
+                  <div className="content" dangerouslySetInnerHTML={{ __html: elem.content }}>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
         ))}
     </div>
   )
