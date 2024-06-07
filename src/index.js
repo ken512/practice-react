7b14491f0cfb237f105934c6643e4328bb5aff49
porota <ken887265@gmail.com>
2024-06-07 18:01:56 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { posts } from './data/posts';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App src={posts} />
+      <App />
       </BrowserRouter>
   </React.StrictMode>
 );
