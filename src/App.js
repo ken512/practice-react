cf4117313c85952f7654c6bcd3ef5b0984b2526a
porota <ken887265@gmail.com>
2024-06-19 15:59:40 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<PostsList />} />
       <Route path="/post/:id" element={<DetailsPage  />} />
-      <Route path="/Inquiry" element={<InquiryPage />} />
+      <Route path="/inquiry" element={<InquiryPage />} />
     </Routes>
   );
 
